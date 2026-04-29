67135f9d321ba4c018c6713056329656de0bb297
ARUN <aryan.arunachalam@gmail.com>
2026-04-29 02:51:17 -0400

    Create 6_parsing_json.py

---
+++ api/6_parsing_json.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+response = requests.get(url)
+data = response.json()
+
+for post in data[:5]:
+    print("ID:", post["id"])
+    print("Title:", post["title"])
+    print("-" * 30)
